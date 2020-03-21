c2e87ba5c5c4a0dd50f877c6abb83e2c52b10aaa
Danny Haro <danny.dhs@gmail.com>
2020-03-21 17:10:32 -0500

    Terminando formulario de busqueda

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -7,15 +7,17 @@ def formulario_buscar(request):
 
 
 def buscar(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Por favor introduce un término de búsqueda.')
+        elif len(q) > 20:
+            errors.append('Por favor introduce un término de búsqueda menor a 20 caracteres.')
         else:
             libros = Libro.objects.filter(titulo__icontains=q)
             return render(request, 'resultados.html', {'libros': libros, 'query':q})
 
-    return render(request, 'formulario_buscar.html', {'error': error})
+    return render(request, 'formulario_buscar.html', {'errors': errors})
 
 
